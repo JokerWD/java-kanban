ccdfc64ead01df8eb8f6f545ab243229bd2c7a3f
joker_Wd <joker.osnova1902@gmail.com>
2024-09-21 20:16:23 +0300

    -fix dir

--- test/service/ManagersTest.java
+++ test/service/ManagersTest.java
@@ -14,14 +14,12 @@ class ManagersTest {
     public void testGetDefaultTaskManager() {
         TaskManager taskManager = Managers.getDefault();
         assertNotNull(taskManager, "Таск менеджер не должен быть null");
-        assertInstanceOf(InMemoryTaskManager.class, taskManager, "Таск менеджер реализует класс InMemoryTaskManager");
     }
 
     @Test
     public void testGetDefaultHistoryManager() {
         HistoryManager historyManager = Managers.getDefaultHistory();
         assertNotNull(historyManager, "Таск менеджер не должен быть null");
-        assertInstanceOf(InMemoryHistoryManager.class, historyManager, "Менеджер историй должен  реализовывать InMemoryHistoryManager");
     }
 
 }
